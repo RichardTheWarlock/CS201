76afb55cfd1322afda82bdf67cfc065598d8f063
Richard <Richard@DESKTOP-RFPTVC3>
2021-09-01 21:09:00 -0800

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,28 @@
+/**
+HW0
+Richard Ruebling
+CS201
+09/01/2021
+**/
+
+#include<iostream>
+using std::cout;
+using std::endl;
+
+int i = 1;
+
+int main()
+{
+	int i = 1;
+	cout << "Main Program:" << endl;
+	while (i <= 5)
+	{
+		cout << "Hello World!" << endl;
+		++i;
+	}
+	cout << "Poem:" << endl;
+	cout << "I caught a big fish, Now I search for a friend, to share my lunch with." << endl;
+	cout << "Game Description:" << endl;
+	cout << "The World's greatest roleplaying game." << endl;
+		return 0;
+}
